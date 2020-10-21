70c4807828ba0ab02c98cf0ad48a9a880c6cb1f0
mccray austin <tubach75@hotmail.com>
2020-10-21 12:24:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
   end
 
   def artist_name=(name)
-    Artist.all.find {|artist| artist.name == name} || new_artist.artist = Artist.new(name)
+    Artist.all.find {|artist| artist.name == name} || song.artist = Artist.new(name)
   end
 
 end
